870fc2b4b8038f06d5065312af22b61006aabb21
marynabukayeva <mb7068@gmail.com>
2022-12-31 20:11:16 +0100

    uncommented

--- src/Walk.java
+++ src/Walk.java
@@ -1,20 +1,24 @@
 public class Walk {
 
+    static boolean isEdekaOpen = true;
+    static boolean isReweOpen = true;
+
+    static boolean canBuy() {
+        if (isEdekaOpen == true || isReweOpen == true) {
+            return true;
+
+        } else {
+            return false;
+        }
+
+    }
+
     public static void main(String[] args) {
 
-
-        System.out.println(args[0]);
-
-
-
-
-
-
-
-        boolean isWeekend=false;
-        boolean isRain=false;
+        boolean isWeekend = false;
+        boolean isRain = false;
         boolean canWalk = (isWeekend == true && isRain == false) ? true : false;
-
+        System.out.println("Я могу купить еду, это " + ((Walk.canBuy())?"правда":"неправда"));
 
 //        boolean canWalk;
 //        if (isWeekend == true && isRain == false) {
