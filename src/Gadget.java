bc66ea174fc0886daeecdd854db403e55c80c3da
marynabukayeva <mb7068@gmail.com>
2023-01-01 01:13:14 +0100

    uncommented

--- src/Gadget.java
+++ src/Gadget.java
@@ -1,5 +1,10 @@
 public class Gadget {
     static boolean isWorking(int Temperature1, int Temperature2) {
-        return false;
+        if (Temperature1 > 100 && Temperature2 < 100) {
+            return true;
+        } else {
+            return false;
+        }
+
     }
 }
