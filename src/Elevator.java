f6cd65492fa3542fcc2123055f81a8222ccc8673
marynabukayeva <mb7068@gmail.com>
2023-01-02 22:22:18 +0100

    uncommented

--- src/Elevator.java
+++ src/Elevator.java
@@ -1,2 +1,14 @@
 public class Elevator {
+
+    public static int elevator(int h, int n, int m){
+        int floor = 0;
+        int counter = 0;
+        while (floor < h){
+            floor = floor + n - m;
+            counter = counter+1;
+
+        }
+        return counter;
+    }
+
 }
